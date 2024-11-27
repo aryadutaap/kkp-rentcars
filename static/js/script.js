927d0d1726c7dd390650b29526c1e55a45eade33
Arya <aryaduta193@gmail.com>
2024-11-27 22:04:08 +0700

    add : card slider daftar sewa mobil

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// navbar
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
@@ -22,5 +23,42 @@ window.onload = () => {
     document.querySelector("header").classList.add("active");
   } else {
     document.querySelector("header").classList.remove("active");
+  }
+};
+
+// card-cars
+let wrapper = document.querySelector(".wrapper-box");
+let activeBox = document.querySelectorAll(".box");
+let prevBtn = document.querySelector("#prevBtn");
+let nextBtn = document.querySelector("#nextBtn");
+
+let indexNo = 0;
+
+nextBtn.onclick = () => {
+  indexNo++;
+  changeBox();
+};
+
+prevBtn.onclick = () => {
+  indexNo--;
+  changeBox();
+};
+
+let changeBox = () => {
+  if (indexNo > activeBox.length - 1) {
+    indexNo = 0;
+  } else if (indexNo < 0) {
+    indexNo = activeBox.length - 1;
+  }
+
+  for (let i = 0; i < activeBox.length; i++) {
+    if (i == indexNo) {
+      activeBox[i].classList.add("active");
+      if (window.screen.width > 768) {
+        wrapper.style.transform = `translateX(${indexNo * -22.5}vw)`;
+      }
+    } else {
+      activeBox[i].classList.remove("active");
+    }
   }
 };
